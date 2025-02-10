cfdc951f7564d984b2cc3ce015279d94ed04e196
chai-dev682 <chai.dev682@gmail.com>
2025-02-10 07:47:54 +0000

    chore: add name show on headline

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -6,9 +6,9 @@ const Hero = (): JSX.Element => {
   return (
     <div className="flex flex-col-reverse md:flex-row md:justify-between md:space-y-0">
       <div className="mt-8 flex flex-col justify-center space-y-4 md:mt-0 md:w-2/3">
-        <h1 className="will-change-opacity text-5xl font-bold will-change-transform">
+        {process.env.NEXT_PUBLIC_NAME_SHOW_HEADLINE && (<h1 className="will-change-opacity text-5xl font-bold will-change-transform">
           {process.env.NEXT_PUBLIC_FIRST_NAME} {process.env.NEXT_PUBLIC_LAST_NAME}
-        </h1>
+        </h1>)}
         <h2 className="will-change-opacity text-lg font-medium text-gray-300 will-change-transform">
           GenAI Specialist | Backend Engineering | MLOps Engineer | Business Automation | Cloud Architect | 9+ years of experience | Startup Mindset
         </h2>
